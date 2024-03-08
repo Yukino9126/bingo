43096303b7de00ae76982f608736c939304de806
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-03-08 15:11:40 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import server
+import client
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send, receive UDP broadcast')
+    parser.add_argument('role', choices=choices, help='which role to take')
+    parser.add_argument('host', help='interface the server listens at;'
+                        ' network the client sends to')
+    parser.add_argument('-p', metavar='port', type=int, default=1060,
+                        help='UDP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
