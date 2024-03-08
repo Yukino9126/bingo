51cf5687e797dc666b6e66b09c9a04d3f8b39162
yukino <snowlychen2384@gmail.com>
2024-03-08 15:29:31 +0800

    Add comment

--- bingo.py
+++ bingo.py
@@ -1,6 +1,11 @@
 def shuffle():
+    '''
+    Return a list of 25 random numbers.
+    '''
     import random
     mylist = range(1,26)
     random.shuffle(mylist)
     return mylist
 
+def check(bingo):
+    return
