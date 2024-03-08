1efe4788380e3a03f85d64e3d32b733a89888580
yukino <snowlychen2384@gmail.com>
2024-03-08 15:38:47 +0800

    Fix bingo.shuffle()

--- bingo.py
+++ bingo.py
@@ -3,9 +3,19 @@ def shuffle():
     Return a list of 25 random numbers.
     '''
     import random
-    mylist = range(1,26)
+    mylist = list(range(1,26))
+    print(mylist)
     random.shuffle(mylist)
     return mylist
 
-def check(bingo):
+def check(bingo:list):
+    '''
+    Check if a 2-dem list bingo.
+    '''
+    line=0
+
+    for i in range(0,5):
+        #if bingo[][] == 
+        line += 1
+
     return
