488045952320b2ee7b75afb0092ab5f0a7bd6513
資工系 陳嘉璐 <s110321026@lilina.csie.ncnu.edu.tw>
2024-03-08 16:32:19 +0800

    modify

--- server.py
+++ server.py
@@ -2,6 +2,14 @@ import socket
 
 MAX_BYTES =65535
 
+def reclient(name, text, num):
+    
+    for i in range(0,25):
+        recvlist[i] = text[i+1]
+    message = name + ", You are the " + num + 'player\n'
+    return message
+
+
 def server(interface, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((interface, port))
@@ -13,23 +21,11 @@ def server(interface, port):
         try:
             data, address = sock.recvfrom(MAX_BYTES)
             text = data.decode('ascii')
-            # string -> list
-            dim = reclient(text)
-            if dim != '':
-                message = sendclient(num, sock.getsockname())
-                sock.sendto(message.encode('ascii'), address)
+            name, numlist = text.split(',') 
+            message = reclient(name, numlist, num)
+            sock.sendto(message.encode('ascii'), address)
             num +=1
         except socket.timeout as exc:
             print('Timeout: I did not receive any message.')
 
-
-def reclient(text):
-    # delete '/join' and change to 2-dimin
-    for i in range(0,25):
-        recvlist[i] = text[i+1]
-    return dim
-
-def sendclient(num, name):
-    message = name + ", You are the " + num + 'player\n'
-    return message
 
