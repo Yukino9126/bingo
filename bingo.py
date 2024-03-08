71209e2c3b7a87164953fc3d5d524dd81c1bbb8e
yukino <snowlychen2384@gmail.com>
2024-03-08 16:15:25 +0800

    Add checklist.

--- bingo.py
+++ bingo.py
@@ -9,14 +9,50 @@ def shuffle():
     string = ','.join([str(i) for i in mylist])
     return string
 
+def gen_checklist():
+    checklist = []
+    for p in range(5):
+
+        # Horizontal
+        nulllist = [False]*25
+        for i in range(5):
+            nulllist[p * 5 + i] = True
+        checklist.append(nulllist)
+
+        # Vertical
+        nulllist = [False]*25
+        for i in range(5):
+            nulllist[p + 5 * i] = True
+        checklist.append(nulllist)
+
+        # Slash
+        nulllist = [False]*25
+        for i in 0, 6, 12, 18, 24:
+            nulllist[i] = True
+        checklist.append(nulllist)
+        nulllist = [False]*25
+        for i in 4, 8, 12, 16, 20:
+            nulllist[i] = True
+        checklist.append(nulllist)
+        
 def check(bingo:list):
     '''
     Check if a 2-dem list bingo.
     '''
     line=0
 
-    for i in range(0,5):
-        #if bingo[][] == 
-        line += 1
+    '''
+    for i in range(0, 5):
+        flag1 = False
+        flag2 = False
+
+        for j in range(0, 5):
+            if bingo[i][j] == bingo[i][j]
+                flag1 = True
+            if bingo[i][j] == bingo[i][j]
+                flag2 = True
+        if flag1: line += 1
+        if flag2: line += 1
+    '''
 
     return
