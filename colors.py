4e0e6bcb0b8038d2123cdd632662c0d1cf436d58
資工系 陳嘉璐 <s110321026@lilina.csie.ncnu.edu.tw>
2024-03-16 22:24:04 +0800

    You can see all ANSI color code here.

---
+++ colors.py
@@ -0,0 +1,19 @@
+# forwardground
+fgBlack = '\033[30m'
+fgRed = '\033[31m'
+fgGreen = '\033[32m'
+fgYellow = '\033[33m'
+fgBlue = '\33[34m'
+fgMagenta = '\33[35m'
+fgCyan = '\033[36m'
+# backgroung
+bgBlack = '033[40m'
+bgRed = '\033[41m'
+bgGreen = '\033[42m'
+bgYellow = '\033[43m'
+bgBlue = '\33[44m'
+bgMagenta = '\33[45m'
+bgCyan = '\033[46m'
+bgWhite = '\033[47m'
+# end
+endColor = '\033[0m'
